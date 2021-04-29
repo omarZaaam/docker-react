f3a4328bebec74ebdfe74dffd0d839512c1fd7d7
Omar Azzam <omarazzam@Omars-MacBook-Pro.local>
2021-04-29 02:43:16 -0400

    Updated files

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,10 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/Learning React when commited should be tested by Travis CI/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Learning React when commited should be tested by Travis CI/i);
+  expect(linkElement).toBeInTheDocument();
+});
